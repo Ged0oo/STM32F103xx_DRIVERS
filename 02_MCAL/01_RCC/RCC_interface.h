4e645acdc1688801538d164f2d810e0b49706868
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-02-27 08:25:22 +0200

    Update RCC_interface.h

--- 02_MCAL/01_RCC/RCC_interface.h
+++ 02_MCAL/01_RCC/RCC_interface.h
@@ -1,122 +1,205 @@
 /* 
- * File:   RCC_INTERFACE.h
+ * File:   RCC_interface.h
  * Author: Mohamed_Nagy
  * https://github.com/Ged0oo 
  * https://www.linkedin.com/in/mohamednagyofficial/
  * Created on February 16, 2023, 8:41 PM
  */
 
+#include "BIT_MATH.h"
+#include "STD_TYPES.h"
+#include "RCC_config.h"
+#include "RCC_private.h"
 
 
-#ifndef __RCC_INTERFACE_H__
-#define __RCC_INTERFACE_H__
 
-/* 
- * RCC_ClksState_t Enum to deal with 
- * different clock sources states(PLL , HSI , HSE ) and which passes or not 
- */
+
+#ifndef RCC_INTERFACE_H
+#define RCC_INTERFACE_H
+
+
+
+/* Buses Names */
+#define			RCC_AHB_BUS						0
+#define			RCC_APB1_BUS					1
+#define			RCC_APB2_BUS					2
+
+
+/* System CLOCK SOURCE */
+#define			RCC_HSI							0
+#define			RCC_HSE							1
+#define			RCC_PLL							2
+
+
+/* PLL CLOCK SOURCE */
+#define 		PLL_SRC_HSE_NOT_DEVIDED			0
+#define			PLL_SRC_HSE_DEVIDED_BY_2		1
+#define			PLL_SRC_HSI_DEVIDED_BY_2		2
+
+
+/* RCC Clock SOURCE */
+#define			RCC_DISABLE_PERIPHERAL			0
+#define			RCC_ENABLE_PERIPHERAL			1
+
+
+/* MCO CLOCK SOURCE */
+#define			MCO_No_CLK               		0
+#define			MCO_SYSCLK 				 		1
+#define			MCO_HSI				 	 		2
+#define			MCO_HSE					 		3
+#define			MCO_PLL					 		4
+
+
+/* PLL MUL FACTOR */
+#define 		PLL_x_2_INPUT_CLOCK            	(0x0)
+#define 		PLL_x_3_INPUT_CLOCK            	(0x1)
+#define 		PLL_x_4_INPUT_CLOCK            	(0x2)
+#define 		PLL_x_5_INPUT_CLOCK            	(0x3)
+#define 		PLL_x_6_INPUT_CLOCK            	(0x4)
+#define 		PLL_x_7_INPUT_CLOCK            	(0x5)
+#define 		PLL_x_8_INPUT_CLOCK           	(0x6)
+#define 		PLL_x_9_INPUT_CLOCK            	(0x7)
+#define 		PLL_x_10_INPUT_CLOCK            (0x8)
+#define 		PLL_x_11_INPUT_CLOCK            (0x9)
+#define 		PLL_x_12_INPUT_CLOCK            (0xA)
+#define 		PLL_x_13_INPUT_CLOCK            (0xB)
+#define 		PLL_x_14_INPUT_CLOCK            (0xC)
+#define 		PLL_x_15_INPUT_CLOCK            (0xD)
+#define 		PLL_x_16_INPUT_CLOCK            (0xE)
+#define 		PLL_x_16_2ND_INPUT_CLOCK        (0xF)
+
+
+/* AHB PRESCALER */
+#define 		AHB_NOT_DEVIDED                 (0x0)
+#define 		AHB_DIVIDED_BY_2                (0x8)
+#define 		AHB_DIVIDED_BY_4                (0x9)
+#define 		AHB_DIVIDED_BY_8                (0xA)
+#define 		AHB_DIVIDED_BY_16               (0xB)
+#define 		AHB_DIVIDED_BY_64               (0xC)
+#define 		AHB_DIVIDED_BY_128              (0xD)
+#define 		AHB_DIVIDED_BY_256              (0xE)
+#define 		AHB_DIVIDED_BY_512              (0xF)
+
+
+/* APB1 PRESCALER */
+#define 		APB1_NOT_DEVIDED                (0x0)
+#define 		APB1_DIVIDED_BY_2               (0x4)
+#define 		APB1_DIVIDED_BY_4               (0x5)
+#define 		APB1_DIVIDED_BY_8               (0x6)
+#define 		APB1_DIVIDED_BY_16              (0x7)
+
+
+/* APB2 PRESCALER */
+#define 		APB2_NOT_DEVIDED                (0x0)
+#define 		APB2_DIVIDED_BY_2               (0x4)
+#define 		APB2_DIVIDED_BY_4               (0x5)
+#define 		APB2_DIVIDED_BY_8               (0x6)
+#define 		APB2_DIVIDED_BY_16              (0x7)
+
+
+/* ADC PRESCALER */
+#define 		RCC_ADC_PRESCALER_2				(0x00000000)
+#define 		RCC_ADC_PRESCALER_4				(0x00004000)
+#define 		RCC_ADC_PRESCALER_6				(0x00008000)
+#define 		RCC_ADC_PRESCALER_8				(0x0000C000)
+
+
+/*PERIPHERALS CLOCK NUMBERS*/
 typedef enum
 {
-	RCC_PLL_OFF_HSI_OFF_HSE_OFF_BYPASS_OFF =  0x00,
-	RCC_PLL_OFF_HSI_OFF_HSE_ON_BYPASS_OFF  =  0x02,
-	RCC_PLL_OFF_HSI_ON_HSE_OFF_BYPASS_OFF  =  0x04,
-	RCC_PLL_OFF_HSI_ON_HSE_ON_BYPASS_ON    =  0x07,
-	
-	RCC_PLL_ON_HSI_OFF_HSE_OFF_BYPASS_OFF  =  0x08,
-	RCC_PLL_ON_HSI_OFF_HSE_ON_BYPASS_OFF   =  0x0A,
-	RCC_PLL_ON_HSI_ON_HSE_OFF_BYPASS_OFF   =  0x0C,
-	RCC_PLL_ON_HSI_ON_HSE_ON_BYPASS_ON     =  0x0F
-}RCC_ClksState_t ; 
+	/* RCC AHB PERIPHERALS */
+	RCC_DMA1_CLOCK,
+	RCC_DMA2_CLOCK,
+	RCC_SRAM_CLOCK,
+	RCC_FLITF_CLOCK = 4,
+	RCC_CRCE_CLOCK  = 6,
+	RCC_FSMC_CLOCK  = 8,
+	RCC_SDIO_CLOCK  = 10,
+
+	/* RCC APB1ENR PERIPHERALS */
+	RCC_TIM2_CLOCK = 0,
+	RCC_TIM3_CLOCK,
+	RCC_TIM4_CLOCK,
+	RCC_TIM5_CLOCK,
+	RCC_TIM6_CLOCK,
+	RCC_TIM7_CLOCK,
+	RCC_TIM12_CLOCK,
+	RCC_TIM13_CLOCK,
+	RCC_TIM14_CLOCK,
+	RCC_WWDGCLOCK = 11,
+	RCC_SPI2CLOCK = 14,
+	RCC_SPI3CLOCK,
+	RCC_USART2_CLOCK = 17,
+	RCC_USART3_CLOCK,
+	RCC_USART4_CLOCK,
+	RCC_USART5_CLOCK,
+	RCC_I2C1_CLOCK,
+	RCC_I2C2_CLOCK,
+	RCC_USB_CLOCK,
+	RCC_CAN_CLOCK =25,
+	RCC_BKP_CLOCK =27,
+	RCC_PWR_CLOCK,
+	RCC_DAC_CLOCK,
+
+	/* RCC APB2ENR PERIPHERALS */
+	RCC_AFIO_CLOCK  = 0,
+	RCC_GPIOA_CLOCK = 2,
+	RCC_GPIOB_CLOCK,
+	RCC_GPIOC_CLOCK,
+	RCC_GPIOD_CLOCK,
+	RCC_GPIOE_CLOCK,
+	RCC_GPIOF_CLOCK,
+	RCC_GPIOG_CLOCK,
+	RCC_ADC1_CLOCK = 9,
+	RCC_ADC2_CLOCK,
+	RCC_TIM1_CLOCK,
+	RCC_SPI1_CLOCK,
+	RCC_TIM8_CLOCK,
+	RCC_USART1_CLOCK,
+	RCC_ADC3_CLOCK,
+	RCC_TIM9_CLOCK = 19,
+	RCC_TIM10_CLOCK,
+	RCC_TIM11_CLOCK,
+}RCC_Peripheral_t;
 
 
-/* 
- * RCC_SystemClk_t Enum to deal with 
- * System clock which is directly enter to the processor  
- */
-typedef enum 
+
+/* PRESCALER CONFIGURATION */
+typedef struct
 {
-    RCC_SYSTCLK_HSI = 0x00 , 
-    RCC_SYSTCLK_HSE = 0x01 , 
-    RCC_SYSTCLK_PLL = 0x02 
-}RCC_SystemClk_t ; 
+	uint8 AHB_Prescaler;
+	uint8 ABP1_Prescaler;
+	uint8 ABP2_Prescaler;
+}Buses_Prescaler;
 
 
-/* 
- * RCC_PllInput_t is an Enum to deal with
- * pll input states  
- */
-typedef enum 
+/*PLL CONFIGURATION */
+typedef struct
 {
-    RCC_PLL_IN_HSI_BY_2 = 0x00 , 
-    RCC_PLL_IN_HSE      = 0x01 , 
-    RCC_PLL_IN_HSE_BY_2 = 0x03 ,
-	RCC_PLL_NOT_USED    = 0x04
-}RCC_PllInput_t ; 
+	uint8 PLL_Source;
+	uint8 PLL_Mul_Factor;
+}RCC_PLL_Typedef;
 
-/* 
- * RCC_PllMulFactor_t is an Enum to deal with 
- * pll multiplaction factor 
- */
-typedef enum 
+
+typedef struct
 {
-    PLL_IN_X_4  = 0x02 , 
-    PLL_IN_X_5  = 0x03 , 
-    PLL_IN_X_6  = 0x04 , 
-    PLL_IN_X_7  = 0x05 , 
-    PLL_IN_X_8  = 0x06 , 
-    PLL_IN_X_9  = 0x07 , 
-    PLL_IN_X_10 = 0x08 , 
-    PLL_IN_X_11 = 0x09 , 
-    PLL_IN_X_12 = 0x0A , 
-    PLL_IN_X_13 = 0x0B , 
-    PLL_IN_X_14 = 0x0C , 
-    PLL_IN_X_15 = 0x0D , 
-    PLL_IN_X_16 = 0x0E
-}RCC_PllMulFactor_t ;
+	uint8  ClockSource;
+	RCC_PLL_Typedef PLL_Config;
+	Buses_Prescaler Prescaler;
+}RCC_Config_t;
 
 
 
 
-/* 
- * function name : RCC_voidInitSystemClk
- * return type   : void 
- * paramters     : 
-					1- RCC_ClksState_t    : defines all clock resources states(PLL , HSI , HSE ) and if HSE passes from pins or not 
-											{RCC_PLL_OFF_HSI_OFF_HSE_OFF_BYPASS_OFF , RCC_PLL_OFF_HSI_ON_HSE_OFF_BYPASS_OFF ,
-											 RCC_PLL_OFF_HSI_OFF_HSE_ON_BYPASS_OFF  , RCC_PLL_OFF_HSI_ON_HSE_ON_BYPASS_ON   ,
-											 RCC_PLL_ON_HSI_OFF_HSE_OFF_BYPASS_OFF  , RCC_PLL_ON_HSI_ON_HSE_OFF_BYPASS_OFF  ,
-											 RCC_PLL_ON_HSI_OFF_HSE_ON_BYPASS_OFF   , RCC_PLL_ON_HSI_ON_HSE_ON_BYPASS_ON    }
-				 
-					2- RCC_SystemClk_t    : defines which clock source the system runs on 			   
-											{RCC_SYSTCLK_HSI , RCC_SYSTCLK_HSE , RCC_SYSTCLK_PLL } 
-									   
-					3- RCC_PllInput_t     : defines pll ciruit input 	                   
-											{RCC_PLL_IN_HSI_BY_2 , RCC_PLL_IN_HSE , RCC_PLL_IN_HSE_BY_2 , RCC_PLL_NOT_USED }
-				 
-					4- RCC_PllMulFactor_t : defines pll circuit multiplaction factor 			   
-											{PLL_IN_X_1 , PLL_IN_X_2  , PLL_IN_X_3  , PLL_IN_X_4  ,
-											PLL_IN_X_5  , PLL_IN_X_6  , PLL_IN_X_7  , PLL_IN_X_8  ,
-											PLL_IN_X_9  , PLL_IN_X_10 , PLL_IN_X_11 , PLL_IN_X_12 ,
-											PLL_IN_X_13	, PLL_IN_X_14 , PLL_IN_X_15	, PLL_IN_X_16 }		
-											
- * Description  : initalize the previous paramters. 		   
- */
-void RCC_voidInitSystemClk(RCC_ClksState_t Copy_u8ClkStates , RCC_SystemClk_t Copy_u8SystemClk ,RCC_PllInput_t Copy_u8PllIn ,RCC_PllMulFactor_t Copy_u8Pllmulfactor) ;  
+/* prototypes of functions */
+void RCC_voidClockInit(RCC_Config_t *RCC_Config);
+void RCC_voidPeripheralClockEnable(uint8 Copy_u8BusName, RCC_Peripheral_t Copy_u8PeripheralName);
+void RCC_voidPeripheralClockDisable(uint8 Copy_u8BusName, RCC_Peripheral_t Copy_u8PeripheralName);
 
+static void RCC_voidClockSource(uint8 Copy_u8xClock);
+static void RCC_voidBus_Prescaler(RCC_Config_t *Copy_u8RCC_Config);
+static void RCC_voidPLL_Mul(uint8 Copy_u8xMul_Factor);
+static void RCC_voidClockSource(uint8 Copy_u8xClock);
+static void RCC_voidPLL_Clock_Source(uint8 Copy_u8xPLL_Source);
 
-
-/* 
- * function name : RCC_voidSetClksState
- * return type   : void 
- * paramters     : 
-					1- RCC_ClksState_t    : defines all clock resources states(PLL , HSI , HSE ) and if HSE passes from pins or not 
-											{RCC_PLL_OFF_HSI_OFF_HSE_OFF_BYPASS_OFF , RCC_PLL_OFF_HSI_ON_HSE_OFF_BYPASS_OFF ,
-											 RCC_PLL_OFF_HSI_OFF_HSE_ON_BYPASS_OFF  , RCC_PLL_OFF_HSI_ON_HSE_ON_BYPASS_ON   ,
-											 RCC_PLL_ON_HSI_OFF_HSE_OFF_BYPASS_OFF  , RCC_PLL_ON_HSI_ON_HSE_OFF_BYPASS_OFF  ,
-											 RCC_PLL_ON_HSI_OFF_HSE_ON_BYPASS_OFF   , RCC_PLL_ON_HSI_ON_HSE_ON_BYPASS_ON    }
-											 
- * Description : initalize clocks states. 		   
-*/	
-void RCC_voidSetClksState(RCC_ClksState_t Copy_u8ClkStates) ;
-				   
-#endif  __RCC_INTERFACE_H__
+#endif
