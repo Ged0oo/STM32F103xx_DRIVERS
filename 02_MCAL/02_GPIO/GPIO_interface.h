1f8e90be1367598cf8577562d82a56c1e775a7a5
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-03-07 08:51:26 +0200

    Add files via upload

---
+++ 02_MCAL/02_GPIO/GPIO_interface.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   GPIO.INTERFACE.h
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on February 16, 2023, 8:41 PM
+ */
+
+
+#ifndef __GPIO_CONFIG_H__
+#define __GPIO_CONFIG_H__
+
+#include "TEMPLATE_private.h"
+#include "TEMPLATE_config.h"
+
+
+#endif  __GPIO_CONFIG_H__
