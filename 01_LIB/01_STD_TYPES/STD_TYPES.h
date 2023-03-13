7e3327288ddd564bd46c757b3130b14f92fa6c8f
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-03-13 22:02:47 +0200

    Add files via upload

--- 01_LIB/01_STD_TYPES/STD_TYPES.h
+++ 01_LIB/01_STD_TYPES/STD_TYPES.h
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-/* Auther: AHMED SAMIR */
-/* Date: 27/9/2022 */
-/* Version: V02 */
-
-#ifndef STD_TYPES_H
-#define STD_TYPES_H
-
-typedef unsigned char          u8 ;
-typedef unsigned short int     u16 ;
-typedef unsigned long int      u32 ;
-
-typedef signed char          s8 ;
-typedef signed short int     s16 ;
-typedef signed long int      s32 ;
-
-#define NULL                   (void*)0
-
-
-=======
 /*
  * File:   STD_TYPES.h
  * Author: Mohamed_Nagy
@@ -46,41 +25,7 @@ typedef signed long int      sint32 ;
 #define NULL                   (void*)0
 
 
->>>>>>> a50749486543186b42231336da2f32bf6effdd55
 #endif
-=======
-<<<<<<< HEAD
-/* Auther: AHMED SAMIR */
-/* Date: 27/9/2022 */
-/* Version: V02 */
-
-#ifndef STD_TYPES_H
-#define STD_TYPES_H
-
-typedef unsigned char          u8 ;
-typedef unsigned short int     u16 ;
-typedef unsigned long int      u32 ;
-
-typedef signed char          s8 ;
-typedef signed short int     s16 ;
-typedef signed long int      s32 ;
-
-#define NULL                   (void*)0
-
-
-=======
-/*
- * File:   STD_TYPES.h
- * Author: Mohamed_Nagy
- * https://github.com/Ged0oo
- * https://www.linkedin.com/in/mohamednagyofficial/
- * Created on February 16, 2023, 8:41 PM
- */
-
-
-#ifndef STD_TYPES_H
-#define STD_TYPES_H
-
 #include "BIT_MATH.h"
 #include "STD_TYPES.h"
 
@@ -96,6 +41,4 @@ typedef signed long int      sint32 ;
 #define NULL                   (void*)0
 
 
->>>>>>> a50749486543186b42231336da2f32bf6effdd55
 #endif
->>>>>>> 1f8e90be1367598cf8577562d82a56c1e775a7a5
