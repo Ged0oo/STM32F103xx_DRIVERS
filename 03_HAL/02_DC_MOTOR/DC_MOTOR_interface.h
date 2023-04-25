e6c732b6a3dccb39c844583c9bcb1aba6cc81e2c
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-04-25 13:09:46 +0200

    Add files via upload

---
+++ 03_HAL/02_DC_MOTOR/DC_MOTOR_interface.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   DC_MOTOR_INTERFACE.h
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on February 16, 2023, 8:41 PM
+ */
+
+
+#ifndef __DC_MOTOR_INTERFACE_H__
+#define __DC_MOTOR_INTERFACE_H__
+				   
+#endif  __DC_MOTOR_INTERFACE_H__
