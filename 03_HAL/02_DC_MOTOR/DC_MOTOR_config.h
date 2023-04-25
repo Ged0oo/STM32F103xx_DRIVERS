b7c039f75f75b006e9662a986ec967f2612f63a7
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-04-25 13:10:31 +0200

    Update DC_MOTOR_config.h

--- 03_HAL/02_DC_MOTOR/DC_MOTOR_config.h
+++ 03_HAL/02_DC_MOTOR/DC_MOTOR_config.h
@@ -3,7 +3,7 @@
  * Author: Mohamed_Nagy
  * https://github.com/Ged0oo 
  * https://www.linkedin.com/in/mohamednagyofficial/
- * Created on February 16, 2023, 8:41 PM
+ * Created on April 25, 2023, 8:41 PM
  */
 
 
