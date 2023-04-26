a9e4b5dc8818300248624a63c5ed02513e7b00eb
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-04-26 13:48:22 +0200

    Add files via upload

--- 03_HAL/02_DC_MOTOR/DC_MOTOR_program.c
+++ 03_HAL/02_DC_MOTOR/DC_MOTOR_program.c
@@ -9,6 +9,63 @@
 
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
-#include "TEMPLATE_interface.h"
-#include "TEMPLATE_private.h"
-#include "TEMPLATE_config.h"
+#include "DC_MOTOR_interface.h"
+
+
+
+void dc_motor_initialize(const dc_motor_t *_dc_motor)
+{
+    if(NULL == _dc_motor)
+	{
+        return;
+    }
+    else
+	{    
+    	GPIO_voidInitPortPin(_dc_motor->Pin_1_GPIO_portX , _dc_motor->Pin_1_GPIO_pinX , GPIO_PIN_GENERAL_PURPOSE_OUTPUT_PUSHPULL_MODE_10MHZ );
+    	GPIO_voidInitPortPin(_dc_motor->Pin_2_GPIO_portX , _dc_motor->Pin_2_GPIO_pinX , GPIO_PIN_GENERAL_PURPOSE_OUTPUT_PUSHPULL_MODE_10MHZ );
+    }
+}
+
+
+void dc_motor_move_right(const dc_motor_t *_dc_motor)
+{
+    if(NULL == _dc_motor)
+	{
+        return;
+    }
+    else
+	{
+		GPIO_voidWritePortPin(_dc_motor->Pin_1_GPIO_portX , _dc_motor->Pin_1_GPIO_pinX , GPIO_HIGH);
+		GPIO_voidWritePortPin(_dc_motor->Pin_2_GPIO_portX , _dc_motor->Pin_2_GPIO_pinX , GPIO_LOW );
+	}
+}
+
+
+void dc_motor_move_left(const dc_motor_t *_dc_motor)
+{
+    if(NULL == _dc_motor)
+	{
+        return;
+    }
+    else
+	{
+    	GPIO_voidWritePortPin(_dc_motor->Pin_1_GPIO_portX , _dc_motor->Pin_1_GPIO_pinX , GPIO_LOW );
+    	GPIO_voidWritePortPin(_dc_motor->Pin_2_GPIO_portX , _dc_motor->Pin_2_GPIO_pinX , GPIO_HIGH);
+	}
+}
+
+
+void dc_motor_stop(const dc_motor_t *_dc_motor)
+{
+    if(NULL == _dc_motor)
+	{
+        return;
+    }
+    else
+	{
+    	GPIO_voidWritePortPin(_dc_motor->Pin_1_GPIO_portX , _dc_motor->Pin_1_GPIO_pinX , GPIO_LOW );
+    	GPIO_voidWritePortPin(_dc_motor->Pin_2_GPIO_portX , _dc_motor->Pin_2_GPIO_pinX , GPIO_LOW );
+	}
+}
+
+
