55dfd9d2e5cf33c2b93cf8f84082054805d67071
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-05-05 12:24:54 +0300

    Add files via upload

--- 02_MCAL/02_GPIO/GPIO_config.h
+++ 02_MCAL/02_GPIO/GPIO_config.h
@@ -4,12 +4,11 @@
  * https://github.com/Ged0oo 
  * https://www.linkedin.com/in/mohamednagyofficial/
  * Created on February 16, 2023, 8:41 PM
- * Last Update on March 19, 2023
  */
+
 
 
 #ifndef __GPIO_CONFIG_H__
 #define __GPIO_CONFIG_H__
-
 
 #endif
