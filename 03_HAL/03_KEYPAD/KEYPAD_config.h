bc13b8f50db1d4bdeeef57a0c9ae7672fe785c47
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-05-06 07:59:55 +0300

    Add files via upload

---
+++ 03_HAL/03_KEYPAD/KEYPAD_config.h
@@ -0,0 +1,30 @@
+/* 
+ * File:   TEMPLATE_CONFIG.h
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on MAY 5, 2023, 8:41 PM
+ */
+
+
+
+#ifndef __TEMPLATE_CONFIG_H__
+#define __TEMPLATE_CONFIG_H__
+
+
+#define			ROW_PORT			GPIOA
+#define			ROW_PIN_0			GPIO_PIN_0
+#define			ROW_PIN_1			GPIO_PIN_1
+#define			ROW_PIN_2			GPIO_PIN_2
+#define			ROW_PIN_3			GPIO_PIN_3
+
+
+#define			COLUMN_PORT			GPIOB
+#define			COLUMN_PIN_0		GPIO_PIN_0
+#define			COLUMN_PIN_1		GPIO_PIN_1
+#define			COLUMN_PIN_2		GPIO_PIN_2
+#define			COLUMN_PIN_3		GPIO_PIN_3
+
+
+
+#endif
