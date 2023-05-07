5eabe635b6200e82204285e6d3a86e76b7d580dd
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-05-07 15:31:01 +0300

    Add files via upload

---
+++ 03_HAL/04_LCD/LCD_config.h
@@ -0,0 +1,28 @@
+/* 
+ * File:   LCD_CONFIG.h
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on MAY 5, 2023, 8:41 PM
+ */
+
+
+
+#ifndef __LCD_CONFIG_H__
+#define __LCD_CONFIG_H__
+
+
+
+#define 		LCD_PORT			GPIOB
+
+#define 		LCD_DATA_0			GPIO_PIN_0
+#define 		LCD_DATA_1			GPIO_PIN_1
+#define 		LCD_DATA_2			GPIO_PIN_2
+#define 		LCD_DATA_3			GPIO_PIN_3
+
+#define 		LCD_RS_PIN			GPIO_PIN_4
+#define 		LCD_EN_PIN			GPIO_PIN_5
+
+
+
+#endif
