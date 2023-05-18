b89de6c7990bd48133e04480a234e1b2cc580518
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-05-18 22:19:25 +0300

    Add files via upload

--- 02_MCAL/01_RCC/RCC_config.h
+++ 02_MCAL/01_RCC/RCC_config.h
@@ -4,16 +4,25 @@
  * https://github.com/Ged0oo 
  * https://www.linkedin.com/in/mohamednagyofficial/
  * Created on February 16, 2023, 8:41 PM
- * Last Update on March 19, 2023
  */
 
 
 #ifndef RCC_CONFIG_H
 #define RCC_CONFIG_H
 
+#include "RCC_interface.h"
 
 /*      HSE ByPass define RCC_HSE_BYPASS         */
 /* TWO MODES : HSE_BYPASS_OFF , HSE_BYPASS_ON    */
 #define RCC_HSE_BYPASS        HSE_BYPASS_OFF
+
+
+
+#define			CLOCK_SOURCE_TYPE			RCC_HSE
+#define 		ABP1_PRESCALER_TYPE			APB1_NOT_DEVIDED
+#define 		ABP2_PRESCALER_TYPE			APB2_NOT_DEVIDED
+#define 		AHB_PRESCALER_TYPE			AHB_NOT_DEVIDED
+
+
 
 #endif
