9ce7f6088199239e51e2929aa37ff3e126d52794
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-05-18 22:19:39 +0300

    Add files via upload

--- 02_MCAL/01_RCC/RCC_interface.h
+++ 02_MCAL/01_RCC/RCC_interface.h
@@ -4,7 +4,6 @@
  * https://github.com/Ged0oo 
  * https://www.linkedin.com/in/mohamednagyofficial/
  * Created on February 16, 2023, 8:41 PM
- * Last Update on March 19, 2023
  */
 
 #include "BIT_MATH.h"
