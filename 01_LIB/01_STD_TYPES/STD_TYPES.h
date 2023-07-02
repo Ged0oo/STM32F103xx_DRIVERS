6ca314cc2edd271e3acddc5a143412e3ef9b47f7
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-07-02 13:36:43 +0300

    Update STD_TYPES.h

--- 01_LIB/01_STD_TYPES/STD_TYPES.h
+++ 01_LIB/01_STD_TYPES/STD_TYPES.h
@@ -24,21 +24,4 @@ typedef signed long int      sint32 ;
 
 #define NULL                   (void*)0
 
-
-#endif
-#include "BIT_MATH.h"
-#include "STD_TYPES.h"
-
-
-typedef unsigned char          uint8 ;
-typedef unsigned short int     uint16 ;
-typedef unsigned long int      uint32 ;
-
-typedef signed char          sint8 ;
-typedef signed short int     sint16 ;
-typedef signed long int      sint32 ;
-
-#define NULL                   (void*)0
-
-
 #endif
