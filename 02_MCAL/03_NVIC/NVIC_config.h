7b1f8cbd0cbe7a7ac5cd694c610737368fd9ba35
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-07-03 22:45:38 +0300

    Add files via upload

---
+++ 02_MCAL/03_NVIC/NVIC_config.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   NVIC_CONFIG.h
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on July 2, 2023, 2:04 PM
+ */
+
+
+
+#ifndef __NVIC_CONFIG_H__
+#define __NVIC_CONFIG_H__
+
+#define 		NVIC_N_IRQNS					59
+
+
+
+#endif  
