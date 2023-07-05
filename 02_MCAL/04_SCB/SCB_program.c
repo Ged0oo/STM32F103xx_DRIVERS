c0a3d1ebf5aa3a6beb3da99a51ddd2d4152a5d62
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-07-05 07:32:59 +0300

    Add files via upload

---
+++ 02_MCAL/04_SCB/SCB_program.c
@@ -0,0 +1,14 @@
+/* 
+ * File:   SCB_PROGRAM.c
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on February 16, 2023, 8:41 PM
+ */
+
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "SCB_interface.h"
+#include "SCB_private.h"
+#include "SCB_config.h"
