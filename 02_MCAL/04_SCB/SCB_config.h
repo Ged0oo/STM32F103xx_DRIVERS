50af87d90c3fa20c5744b1c8246b4cae48fe358f
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-07-05 07:32:37 +0300

    Add files via upload

---
+++ 02_MCAL/04_SCB/SCB_config.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   SCB_CONFIG.h
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on February 16, 2023, 8:41 PM
+ */
+
+
+
+#ifndef __SCB_CONFIG_H__
+#define __SCB_CONFIG_H__
+
+#endif  __SCB_CONFIG_H__
