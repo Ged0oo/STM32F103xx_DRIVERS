8fbeee91af31435b931ec1024f3db832a2e3a7d4
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-07-05 07:33:25 +0300

    Add files via upload

---
+++ 02_MCAL/04_SCB/SCB_interface.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   SCB_INTERFACE.h
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on February 16, 2023, 8:41 PM
+ */
+
+
+#ifndef __SCB_INTERFACE_H__
+#define __SCB_INTERFACE_H__
+				   
+#endif  __SCB_INTERFACE_H__
