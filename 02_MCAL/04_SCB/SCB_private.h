0028a498060364d262011cf8976e4d60f2f99d88
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-07-05 07:33:14 +0300

    Add files via upload

---
+++ 02_MCAL/04_SCB/SCB_private.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   SCB_PRIVATE.h
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on February 16, 2023, 8:41 PM
+ */
+
+
+#ifndef __SCB_PRIVATE_H__
+#define __SCB_PRIVATE_H__
+				   
+#endif  __SCB_PRIVATE_H__
