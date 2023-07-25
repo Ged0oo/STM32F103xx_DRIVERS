f82ff81e409e4a781452b38e60d20b805ef23382
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-07-25 15:05:57 +0300

    Add files via upload

--- 03_HAL/04_LCD/LCD_config.h
+++ 03_HAL/04_LCD/LCD_config.h
@@ -13,15 +13,16 @@
 
 
 
-#define 		LCD_PORT			GPIOB
+#define 		LCD_PORT			GPIOA
 
-#define 		LCD_DATA_0			GPIO_PIN_0
-#define 		LCD_DATA_1			GPIO_PIN_1
-#define 		LCD_DATA_2			GPIO_PIN_2
-#define 		LCD_DATA_3			GPIO_PIN_3
+#define 		LCD_DATA_4			GPIO_PIN_0
+#define 		LCD_DATA_5			GPIO_PIN_1
+#define 		LCD_DATA_6			GPIO_PIN_2
+#define 		LCD_DATA_7			GPIO_PIN_3
 
 #define 		LCD_RS_PIN			GPIO_PIN_4
-#define 		LCD_EN_PIN			GPIO_PIN_5
+#define 		LCD_RW_PIN			GPIO_PIN_5
+#define 		LCD_EN_PIN			GPIO_PIN_6
 
 
 
