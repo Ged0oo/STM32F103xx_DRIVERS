de23450df479ceec1b3892fc71e57cc6335aa1d1
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-08-02 06:10:16 +0300

    Add files via upload

--- 02_MCAL/02_GPIO/GPIO_program.c
+++ 02_MCAL/02_GPIO/GPIO_program.c
@@ -6,26 +6,36 @@
  * Created on February 16, 2023, 8:41 PM
  */
 
+
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
 #include "GPIO_interface.h"
 #include "GPIO_private.h"
 #include "GPIO_config.h"
 
-static void GPIO_voidConfgPin_Mode(GPIO_t *GPIOx , uint8 Copy_u8Pin , uint8 Copy_u8Mode);
+static void MGPIO_voidConfgPin_Mode(GPIO_t *GPIOx , uint8 Copy_u8Pin , uint8 Copy_u8Mode);
 
-void GPIO_voidInitPortPin(GPIO_ConfigType *_gpio)
+
+void MGPIO_voidInitPortPin(GPIO_ConfigType *_gpio)
 {
 	uint8 Copy_u8Pin  = _gpio->GPIO_PinNumber;
 	uint8 Copy_u8Mode = _gpio->GPIO_PinMode;
-	if(Copy_u8Pin > 15) return;
+	if(Copy_u8Pin > 15)
+	{
+		return;
+	}
 	GPIO_voidConfgPin_Mode(_gpio->GPIOx , Copy_u8Pin , Copy_u8Mode);
 	GPIO_voidWritePortPin(_gpio , _gpio->GPIO_Logic);
 }
 
-static void GPIO_voidConfgPin_Mode(GPIO_t *GPIOx , uint8 Copy_u8Pin , uint8 Copy_u8Mode)
+
+static void MGPIO_voidConfgPin_Mode(GPIO_t *GPIOx , uint8 Copy_u8Pin , uint8 Copy_u8Mode)
 {
-	if(Copy_u8Pin > 15) return;
+	if(Copy_u8Pin > 15)
+	{
+		return;
+	}
+
 	if (Copy_u8Pin <= 7)
 	{
 		/* clear bits */
@@ -47,15 +57,33 @@ static void GPIO_voidConfgPin_Mode(GPIO_t *GPIOx , uint8 Copy_u8Pin , uint8 Copy
 	}
 }
 
-void GPIO_voidWritePortPin(GPIO_ConfigType *_gpio , uint8 Copy_u8Val)
+
+
+void MGPIO_voidWritePortPin(GPIO_ConfigType *_gpio , uint8 Copy_u8Val)
 {
-	if(_gpio->GPIO_PinNumber > 15) return;
-	if(Copy_u8Val == GPIO_HIGH) _gpio->GPIOx->BSRR = 1 << (_gpio->GPIO_PinNumber);
-	else _gpio->GPIOx->BRR  = 1 << (_gpio->GPIO_PinNumber);
+	if(_gpio->GPIO_PinNumber > 15)
+	{
+		return;
+	}
+
+	if(Copy_u8Val == GPIO_HIGH)
+	{
+		_gpio->GPIOx->BSRR = 1 << (_gpio->GPIO_PinNumber);
+	}
+	else
+	{
+		_gpio->GPIOx->BRR  = 1 << (_gpio->GPIO_PinNumber);
+	}
 }
 
-uint8 GPIO_u8ReadPortPin(GPIO_ConfigType *_gpio)
+
+uint8 MGPIO_u8ReadPortPin(GPIO_ConfigType *_gpio)
 {
-	if(_gpio->GPIO_PinNumber > 15) return 0;
+	if(_gpio->GPIO_PinNumber > 15)
+	{
+		return 0;
+	}
+
 	return GET_BIT(_gpio->GPIOx->IDR , _gpio->GPIO_PinNumber);
 }
+
