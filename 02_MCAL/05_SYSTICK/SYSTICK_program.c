20bdfd306474ca34aa605f0f770861045d724e64
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-08-08 19:31:00 +0300

    Add files via upload

---
+++ 02_MCAL/05_SYSTICK/SYSTICK_program.c
@@ -0,0 +1,14 @@
+/* 
+ * File:   TEMPLATE_PROGRAM.c
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on February 16, 2023, 8:41 PM
+ */
+
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "TEMPLATE_interface.h"
+#include "TEMPLATE_private.h"
+#include "TEMPLATE_config.h"
