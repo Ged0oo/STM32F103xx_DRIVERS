bce3ab26817a9fba66d2c185cd4949e6ce6d9d17
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-08-16 10:44:32 +0300

    Add files via upload

---
+++ 02_MCAL/06_USART/USART_config.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   USART_CONFIG.h
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on Augest 16, 2023, 8:41 PM
+ */
+
+
+
+#ifndef __USART_CONFIG_H__
+#define __USART_CONFIG_H__
+
+#endif  __USART_CONFIG_H__
