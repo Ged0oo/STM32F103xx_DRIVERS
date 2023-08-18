e04c6760462e7f97411f3129cd632919b086b823
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-08-18 17:31:15 +0300

    Add files via upload

---
+++ 02_MCAL/06_Timers/TIMERS_private.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   TIMERS_PRIVATE.h
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on Augest 18, 2023, 8:41 PM
+ */
+
+
+#ifndef __TIMERS_PRIVATE_H__
+#define __TIMERS_PRIVATE_H__
+				   
+#endif  __TIMERS_PRIVATE_H__
