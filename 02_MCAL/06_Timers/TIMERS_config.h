50e492c5915f3c372f8030c0cec0616dc08155a9
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-08-18 17:30:49 +0300

    Add files via upload

---
+++ 02_MCAL/06_Timers/TIMERS_config.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   TIMERS_CONFIG.h
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on Augest 18, 2023, 8:41 PM
+ */
+
+
+
+#ifndef __TIMERS_CONFIG_H__
+#define __TIMERS_CONFIG_H__
+
+#endif  __TIMERS_CONFIG_H__
