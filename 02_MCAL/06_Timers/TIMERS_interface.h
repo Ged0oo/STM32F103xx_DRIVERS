8dd28b53791d85c112571daf66aaf32c4bb0770e
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-08-18 17:31:01 +0300

    Add files via upload

---
+++ 02_MCAL/06_Timers/TIMERS_interface.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   TIMERS_INTERFACE.h
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on Augest 18, 2023, 8:41 PM
+ */
+
+
+#ifndef __TIMERS_INTERFACE_H__
+#define __TIMERS_INTERFACE_H__
+				   
+#endif  __TIMERS_INTERFACE_H__
