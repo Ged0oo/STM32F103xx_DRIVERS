5e9aa095cdfe25f7965f1d3c5ead64a802ae8a7d
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-08-18 17:31:26 +0300

    Add files via upload

---
+++ 02_MCAL/06_Timers/TIMERS_program.c
@@ -0,0 +1,14 @@
+/* 
+ * File:   TIMERS_PROGRAM.c
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on Augest 18, 2023, 8:41 PM
+ */
+
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "TIMERS_interface.h"
+#include "TIMERS_private.h"
+#include "TIMERS_config.h"
