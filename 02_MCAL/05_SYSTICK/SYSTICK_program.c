3a28f65b2fc4b0890da88a1eb614c89b38e5b6a8
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-09-30 23:07:44 +0300

    Update SYSTICK_program.c

--- 02_MCAL/05_SYSTICK/SYSTICK_program.c
+++ 02_MCAL/05_SYSTICK/SYSTICK_program.c
@@ -16,6 +16,7 @@
 
 static volatile uint8 STK_u8IntervalMode;
 
+
 #define STK_SET_INTERVAL_MODE(MODE)                          STK_u8IntervalMode = MODE
 #define STK_GET_INTERVAL_MODE()                             (STK_u8IntervalMode & 1)
 
